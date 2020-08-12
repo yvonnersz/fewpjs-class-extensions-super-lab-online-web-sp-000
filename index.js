cdcf334a863c227de4912c5c8a1eb1e2d7a3025f
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-12 07:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ class Deciduous extends Tree{
     super(species);
     this.name = name;
   }
+
+  static definition() {
+    return `super.definition() Deciduous trees shed their leaves annually.`
+  }
 }

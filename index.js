3684b8a08209e701a068fa8a6b7307557ea8f01b
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-12 07:17:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,14 @@ class Deciduous extends Tree{
     return `${super.definition()} Deciduous trees shed their leaves annually.`
   }
 }
+
+class Evergreen extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return `${super.definition()} Evergreens keep their leaves all year round.`
+  }
+}
